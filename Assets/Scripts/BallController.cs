52540dd804a2214ca44f074d8a9fe974ebc0f160
prateek-balani <prateekbalani03@gmail.com>
2025-01-22 09:45:57 -0800

    added scripts

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Calling the Start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Calling the Update method");
+    }
+}
