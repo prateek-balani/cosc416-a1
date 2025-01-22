8bb8b168b73440388b0c52511724602fc62ff254
prateek-balani <prateekbalani03@gmail.com>
2025-01-22 12:19:16 -0800

    assignment complete

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,24 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         Vector2 inputVector = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        OnMove?.Invoke(inputVector);
     }
 }
