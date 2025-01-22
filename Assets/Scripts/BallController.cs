49a2219260db1a84c7314423c3c73ef761d81a82
prateek-balani <prateekbalani03@gmail.com>
2025-01-22 11:35:07 -0800

    new script

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,34 +1,47 @@
 using UnityEngine;
-
+using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
+      [SerializeField] private Rigidbody sphereRigidbody;
+      [SerializeField] private float ballSpeed = 2f;
+      
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the Start method");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+      
         Vector2 inputVector = Vector2.zero;
 
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
             inputVector += Vector2.up;
         }
-        if(Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKey(KeyCode.S))
         {
             inputVector += Vector2.down;
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKey(KeyCode.A))
         {
             inputVector += Vector2.left;
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKey(KeyCode.D))
         {
             inputVector += Vector2.right;
         }
+       
         Debug.Log("Resultant Vector: " + inputVector);
+     
+        
     }
+    public void MoveBall(Vector2 input) 
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+     
 }
